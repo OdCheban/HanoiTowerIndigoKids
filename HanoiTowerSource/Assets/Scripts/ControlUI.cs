94918ce39b4776d908a7699af4a52ad40c21d559
Oleg Cheban <odcheban@gmail.com>
2018-08-10 11:58:54 +0300

    InputProtect

---
+++ HanoiTowerSource/Assets/Scripts/ControlUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlUI : MonoBehaviour {
+    public ControlGame controlGame;
+    public Text textField;
+    public InputField Input;
+    public Button btnStart;
+    int n;
+
+    public void StartGame()
+    {
+        controlGame.Kdisk = n;
+    }
+
+    public void ReadField()
+    {
+        try
+        {
+            n = Int32.Parse(Input.text);
+            if (n == 0) throw new Exception();
+            btnStart.interactable = true;
+        }
+        catch 
+        {
+            btnStart.interactable = false;
+            Input.text = "";
+        }
+    }
+}
